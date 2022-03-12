33c884312d77b8e16db2d18d701c9ae4ecc7c1df
Ashu Sharma <ashutosh.sharma@armezo.com>
2022-03-13 00:56:54 +0530

    update with register code

---
+++ controllers/auth/registerController.js
@@ -0,0 +1,9 @@
+const registerController = {
+
+    register(req, res, next) {
+        res.send("this is regiter functions");
+    }
+
+}
+
+export default registerController;
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import routes from './routes';
 import { APP_PORT } from './config';
 
 const app = express();
+
+// Used All routes
+app.use('/api', routes);
 
 app.listen(APP_PORT, ()=> {
     console.log(`This application is running on port${APP_PORT}`);
